42ed59262978ebdb9b0e2e2702a2786f22a73354
Alexander Kholodovitch <kholodovitch@gmail.com>
2014-06-10 21:57:56 +0400

    Add empty ProtoTransitionsActivity.

---
+++ proto/main/src/com/wedeko/visuscreen/player/proto/main/activities/ProtoTransitionsActivity.java
@@ -0,0 +1,13 @@
+package com.wedeko.visuscreen.player.proto.main.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProtoTransitionsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(com.wedeko.visuscreen.player.proto.main.R.layout.activity_proto_transitions);
+	}
+}
