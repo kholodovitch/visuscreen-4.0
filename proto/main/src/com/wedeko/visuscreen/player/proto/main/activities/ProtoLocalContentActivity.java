df6dc82be29693bc3081bf9329638c221cc13f47
Alexander Kholodovitch <kholodovitch@gmail.com>
2014-06-11 13:04:18 +0400

    Implement playing video in WebView element.

--- proto/main/src/com/wedeko/visuscreen/player/proto/main/activities/ProtoLocalContentActivity.java
+++ proto/main/src/com/wedeko/visuscreen/player/proto/main/activities/ProtoLocalContentActivity.java
@@ -2,32 +2,70 @@ package com.wedeko.visuscreen.player.proto.main.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.webkit.WebView;
 
+import com.wedeko.visuscreen.player.proto.R;
 import com.wedeko.visuscreen.player.proto.main.PlayerApplication;
+import com.wedeko.visuscreen.player.proto.main.controls.VideoEnabledWebChromeClient;
+import com.wedeko.visuscreen.player.proto.main.controls.VideoEnabledWebView;
 
 public class ProtoLocalContentActivity extends Activity {
+
+	private VideoEnabledWebView webView;
+	private VideoEnabledWebChromeClient webChromeClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		WebView webView = new WebView(this);
-		webView.setClickable(true);
 
-		WebSettings wSettings = webView.getSettings();
-		//wSettings.setJavaScriptEnabled(true);
-		wSettings.setUseWideViewPort(true);
-		wSettings.setLoadWithOverviewMode(true);
+		// Set layout
+		setContentView(R.layout.activity_proto_local_content);
 
-		WebClientClass webViewClient = new WebClientClass();
-		webView.setWebViewClient(webViewClient);
-		WebChromeClient webChromeClient = new WebChromeClient();
+		// Save the web view
+		webView = (VideoEnabledWebView) findViewById(R.id.webView);
+		webView.getSettings().setUseWideViewPort(true);
+		webView.getSettings().setLoadWithOverviewMode(true);
+
+		// Initialize the VideoEnabledWebChromeClient and set event handlers
+		View nonVideoLayout = findViewById(R.id.nonVideoLayout); // Your own view, read class comments
+		ViewGroup videoLayout = (ViewGroup) findViewById(R.id.videoLayout); // Your own view, read class comments
+		webChromeClient = new VideoEnabledWebChromeClient(nonVideoLayout, videoLayout, null, webView) // See all available constructors...
+		{
+			// Subscribe to standard events, such as onProgressChanged()...
+			@Override
+			public void onProgressChanged(WebView view, int progress) {
+				// Your code...
+			}
+		};
+		webChromeClient.setOnToggledFullscreen(new VideoEnabledWebChromeClient.ToggledFullscreenCallback() {
+			@Override
+			public void toggledFullscreen(boolean fullscreen) {
+				// Your code to handle the full-screen change, for example showing and hiding the title bar. Example:
+				if (fullscreen) {
+					WindowManager.LayoutParams attrs = getWindow().getAttributes();
+					attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+					attrs.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+					getWindow().setAttributes(attrs);
+					if (android.os.Build.VERSION.SDK_INT >= 14) {
+						getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+					}
+				} else {
+					WindowManager.LayoutParams attrs = getWindow().getAttributes();
+					attrs.flags &= ~WindowManager.LayoutParams.FLAG_FULLSCREEN;
+					attrs.flags &= ~WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
+					getWindow().setAttributes(attrs);
+					if (android.os.Build.VERSION.SDK_INT >= 14) {
+						getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+					}
+				}
+
+			}
+		});
 		webView.setWebChromeClient(webChromeClient);
 
 		webView.loadUrl("file://" + PlayerApplication.getStorageDirectory() + "/index.html");
-		setContentView(webView);
 	}
 }
--- proto/main/src/com/wedeko/visuscreen/player/proto/main/activities/WebClientClass.java
+++
@@ -1,7 +0,0 @@
-package com.wedeko.visuscreen.player.proto.main.activities;
-
-import android.webkit.WebViewClient;
-
-public class WebClientClass extends WebViewClient {
-
-}
