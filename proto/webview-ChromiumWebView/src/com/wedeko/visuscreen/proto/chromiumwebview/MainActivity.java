c305b3da42e729867070735b4ead59b7e298b9bb
Alexander Kholodovitch <kholodovitch@gmail.com>
2014-06-18 12:31:19 +0400

    Use local file with transition in ChromiumWebView-prototype.

--- proto/webview-ChromiumWebView/src/com/wedeko/visuscreen/proto/chromiumwebview/MainActivity.java
+++ proto/webview-ChromiumWebView/src/com/wedeko/visuscreen/proto/chromiumwebview/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		WebView webView = (WebView) findViewById(R.id.webView);
-		webView.loadUrl("http://home.kholodovitch.com/test/html5-transitions/transition_horizontalWipe.html");
+		webView.loadUrl("/storage/emulated/0/VisuScreen/transition_fade.html");
 		webView.getSettings().setUseWideViewPort(true);
 		webView.getSettings().setLoadWithOverviewMode(true);
 		webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
